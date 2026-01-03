b6f1a07b27900c7f1e3d5b7325ffd8368992d9eb
Bram Otte <bram@otte.biz>
2026-01-03 13:21:23 +0100

    spsc: Use Acquire and Release orderings

--- crates/sync/mchprs_sync/src/spsc.rs
+++ crates/sync/mchprs_sync/src/spsc.rs
@@ -1,4 +1,4 @@
-use std::{ops::Deref, sync::{Arc, atomic::{AtomicUsize, Ordering}}};
+use std::sync::{Arc, atomic::{AtomicUsize, Ordering}};
 
 use crossbeam_utils::CachePadded;
 
@@ -29,13 +29,13 @@ pub struct Sender<T> {
     read: usize,
 }
 
-impl <T, const SIZE: usize> Sender<[CachePadded<T>; SIZE]> {
+impl <T, const SIZE: usize> Sender<[T; SIZE]> {
     pub fn get(&mut self) -> &mut T {
         let write = self.write;
         let next_write = (write + 1) % SIZE;
         if next_write == self.read {
             loop {
-                self.read = self.queue.read.load(Ordering::Relaxed);
+                self.read = self.queue.read.load(Ordering::Acquire);
                 if self.read != next_write {
                     break;
                 }
@@ -62,7 +62,7 @@ pub struct Receiver<T> {
     read: usize,
 }
 
-impl <T, const SIZE: usize> Receiver<[CachePadded<T>; SIZE]> {
+impl <T, const SIZE: usize> Receiver<[T; SIZE]> {
     pub fn get<'a>(&'a mut self) -> &'a T {
         let read = self.read;
 
@@ -77,6 +77,19 @@ impl <T, const SIZE: usize> Receiver<[CachePadded<T>; SIZE]> {
         }
 
         &self.queue.values[read]
+    }
+
+    pub fn try_get<'a>(&'a mut self) -> Option<&'a T> {
+        let read = self.read;
+
+        if read == self.write {
+            self.write = self.queue.write.load(Ordering::Acquire);
+            if self.write == read {
+                return None;
+            }
+        }
+
+        Some(&self.queue.values[read])
     }
 
     pub fn advance(&mut self) {
@@ -103,77 +116,106 @@ fn test_channel() {
 
     println!();
     println!("get and advance");
+    
+    let (sum, dt1) =
     {
-        let (mut sender, mut receiver) = channel_using::<[CachePadded<Vec<_>>; _]>(
+        let (mut sender, mut receiver) = channel_using::<[Vec<_>; _]>(
             [(); QUEUE_SIZE].map(|_| Default::default())
         );
+        let data: Vec<_> = (0..TO_SEND).map(|v| [v, v+1, v+2]).collect();
     
         let a = std::thread::spawn(move || {
-            let data: Vec<_> = (0..TO_SEND).map(|v| [v, v+1, v+2]).collect();
-            
             let start = Instant::now();
+            let first;
             let sum = black_box({
                 let mut sum = 0u64;
-                for d in data {
+                let d = &data[0];
+                sum += d.iter().sum::<u64>();
+                let s = sender.get();
+                s.clear();
+                s.push(*d);
+                sender.advance();
+                first = Instant::now();
+
+                for d in &data[1..] {
                     sum += d.iter().sum::<u64>();
                     let s = sender.get();
                     s.clear();
-                    s.push(d);
+                    s.push(*d);
                     sender.advance();
                 }
                 sum
             });
-            let dt = Instant::now() - start;
-            println!("write {:?} {} {:e}", dt, sum, TO_SEND as f64 / dt.as_secs_f64());
+            let dt = Instant::now() - first;
+            println!("write {:?} {:?} {} {:e}", first - start, dt, sum, (TO_SEND - 1) as f64 / dt.as_secs_f64());
+
+            return first;
         });
     
         let b = std::thread::spawn(move || {
             let start = Instant::now();
-            let mut first = start;
+            let first;
             let sum = black_box({
                 let mut sum: u64 = 0;
                 let slice = receiver.get();
                 first = Instant::now();
-                for d in slice {
+                for d in slice.iter() {
                     sum += d.iter().copied().sum::<u64>();
                 }
                 receiver.advance();
                 for _ in 1..TO_SEND {
-                    for d in receiver.get() {
+                    for d in receiver.get().iter() {
                         sum += d.iter().copied().sum::<u64>();
                     }
                     receiver.advance();
                 }
                 sum
             });
-            let dt = Instant::now() - first;
-            println!("read {:?} {} {:e}", dt, sum, (TO_SEND - 1) as f64 / dt.as_secs_f64());
+            let end = Instant::now();
+            let dt = end - first;
+            println!("read {:?} {:?} {} {:e}", first - start, dt, sum, (TO_SEND - 1) as f64 / dt.as_secs_f64());
+
+            return (first, sum, dt);
         });
     
-        a.join().unwrap();
-        b.join().unwrap();
-    }
+        let a = a.join().unwrap();
+        let (b, sum, dt) = b.join().unwrap();
+
+        println!("{:?}", b - a);
+
+        (sum, dt)
+    };
 
     println!();
     println!("std");
+
+    let (sum2, dt2) =
     {
         let (sender, receiver) = std::sync::mpsc::sync_channel(QUEUE_SIZE);
+        let data: Vec<_> = (0..TO_SEND).map(|v| [v, v+1, v+2]).collect();
     
         let a = std::thread::spawn(move || {
-            let data: Vec<_> = (0..TO_SEND).map(|v| [v, v+1, v+2]).collect();
 
-    
+            let first;
             let start = Instant::now();
             let sum = black_box({
                 let mut sum: u64 = 0;
-                for d in data {
+                let d = &data[0];
+                sum += d.iter().sum::<u64>();
+                sender.send(vec![*d]).unwrap();
+                first = Instant::now();
+
+                for d in &data[1..] {
                     sum += d.iter().sum::<u64>();
-                    sender.send(vec![d]).unwrap();
+                    sender.send(vec![*d]).unwrap();
                 }
                 sum
             });
-            let dt = Instant::now() - start;
-            println!("write {:?} {} {:e}", dt, sum, TO_SEND as f64 / dt.as_secs_f64());
+            let end = Instant::now();
+            let dt = end - first;
+            println!("write {:?} {:?} {} {:e}", first - start, dt, sum, (TO_SEND - 1) as f64 / dt.as_secs_f64());
+
+            return first;
         });
     
         let b = std::thread::spawn(move || {
@@ -194,11 +236,21 @@ fn test_channel() {
                 }
                 sum
             });
-            let dt = Instant::now() - first;
-            println!("read {:?} {} {:e}", dt, sum, (TO_SEND - 1) as f64 / dt.as_secs_f64());
+            let end = Instant::now();
+            let dt = end - first;
+            println!("read {:?} {:?} {} {:e}", first-start, dt, sum, (TO_SEND - 1) as f64 / dt.as_secs_f64());
+
+            return (first, sum, dt);
         });
     
-        a.join().unwrap();
-        b.join().unwrap();
-    }
+        let a = a.join().unwrap();
+        let (b, sum, dt) = b.join().unwrap();
+
+        println!("{:?}", b - a);
+
+        (sum, dt)
+    };
+
+    assert_eq!(sum, sum2);
+    assert!(dt1 * 5 < dt2);
 }
